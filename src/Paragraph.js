9cac2ec23856ca3cb776e6c688145072de92dcf4
allyjfuller <allyjfuller@gmail.com>
2019-03-07 10:22:48 +0100

    First commit

---
+++ src/Paragraph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lorem from 'react-lorem-component';
+
+class Paragraph extends React.Component {
+	render() {
+		return
+					<h1>Here's my random paragraph: </h1>
+				<div>
+					<Lorem />
+				</div>
+
+	};
+}
+
